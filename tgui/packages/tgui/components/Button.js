04d2f7fab18317455b951f3f0faeb0d62d6f1bb1
BraveMole <bsouchu@gmail.com>
2021-07-09 01:13:30 +0200

    Better keybind registering system

--- tgui/packages/tgui/components/Button.js
+++ tgui/packages/tgui/components/Button.js
@@ -300,3 +300,109 @@ export class ButtonInput extends Component {
 }
 
 Button.Input = ButtonInput;
+<<<<<<< HEAD
+=======
+
+export class ButtonKeybind extends Component {
+  constructor() {
+    super();
+    this.state = {
+      focused: false,
+      keysDown: {},
+    };
+  }
+
+  preventPassthrough(key) {
+    key.event.preventDefault();
+  }
+
+  doFinish() {
+    const { onFinish } = this.props;
+    const { keysDown } = this.state;
+
+    const listOfKeys
+    = Object.keys(keysDown)
+      .filter(isTrue => keysDown[isTrue]);
+
+    onFinish(listOfKeys);
+    document.activeElement.blur();
+    clearInterval(this.timer);
+  }
+
+  handleKeyPress(e) {
+    const { keysDown } = this.state;
+
+    e.preventDefault();
+
+    let pressedKey = e.key.toUpperCase();
+
+    this.finishTimerStart(200);
+
+    // Prevents repeating
+    if (keysDown[pressedKey] && e.type === "keydown") {
+      return;
+    }
+
+    if (e.keyCode >= 96 && e.keyCode <= 105) {
+      pressedKey = "Numpad" + pressedKey;
+    }
+
+    keysDown[pressedKey] = e.type === "keydown";
+    this.setState({
+      keysDown: keysDown,
+    });
+  }
+
+  finishTimerStart(time) {
+    clearInterval(this.timer);
+    this.timer = setInterval(() => this.doFinish(), time);
+  }
+
+  doFocus() {
+    this.setState({
+      focused: true,
+      keysDown: {},
+    });
+    this.finishTimerStart(2000);
+    globalEvents.on('keydown', this.preventPassthrough);
+  }
+
+  doBlur() {
+    this.setState({
+      focused: false,
+      keysDown: {},
+    });
+    globalEvents.off('keydown', this.preventPassthrough);
+  }
+
+  render() {
+    const { focused, keysDown } = this.state;
+    const {
+      content,
+      ...rest
+    } = this.props;
+
+    return (
+      <Button
+        {...rest}
+        content={focused
+          ? Object.keys(keysDown)
+            .filter(isTrue => keysDown[isTrue])
+            .join("+") || content
+          : content}
+        selected={focused}
+        inline
+        onClick={e => {
+          if (focused && Object.keys(keysDown).length) {
+            this.doFinish();
+            e.preventDefault();
+          }
+        }}
+        onFocus={() => this.doFocus()}
+        onBlur={() => this.doBlur()}
+        onKeyDown={e => this.handleKeyPress(e)}
+      />
+    );
+  }
+}
+>>>>>>> 7b644027d (better keybind registering (#7298))
